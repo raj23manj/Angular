63a79b38e92b536721d59e90cceae124319a51fd
rajesh23-drg <rmanjunath@teamdrg.com>
2018-01-19 10:03:49 +0530

    simple directives

--- src/app/courses-manual.component.ts
+++ src/app/courses-manual.component.ts
@@ -4,10 +4,18 @@ import { Component } from '@angular/core'
 // decorator function
 @Component({
   selector: 'courses-manual', // <courses></courses>
-  template: '<h2>{{ getTitle() }}</h2>'
+  template: `
+              <h2>{{ getTitle() }}</h2>
+              <ul>
+                <li *ngFor="let course of courses">
+                  {{ course }}
+                </li>
+              </ul>
+            `
 })
 export class CoursesManualComponent {
- title = "List of courses"
+ title = "List of courses";
+ courses = ["course1", "course2", "course3"]
 
  getTitle() {
    return this.title;
