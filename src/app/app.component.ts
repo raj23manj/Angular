81a38fb52bcca63f247fbf42d798938a802c4135
rajesh23-drg <rmanjunath@teamdrg.com>
2018-01-23 09:09:30 +0530

    ngStyle and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,27 +32,29 @@ export class AppComponent {
   //             { id: 3, name: 'course3'}
   //           ];
 
-  courses;
+  // courses;
+  //
+  // onAdd() {
+  //   this.courses.push({ id: 4, name: 'course4'});
+  // }
+  //
+  // onRemove(course) {
+  //   let index = this.courses.indexOf(course);
+  //   this.courses.splice(index, 1);
+  // }
+  //
+  // loadCourses() {
+  //   this.courses = [
+  //               { id: 1, name: 'course1'},
+  //               { id: 2, name: 'course2'},
+  //               { id: 3, name: 'course3'}
+  //             ];
+  // }
+  //
+  // trackCourse(index, course){
+  //   return course ? course.id : undefined;
+  // }
 
-  onAdd() {
-    this.courses.push({ id: 4, name: 'course4'});
-  }
-
-  onRemove(course) {
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-  }
-
-  loadCourses() {
-    this.courses = [
-                { id: 1, name: 'course1'},
-                { id: 2, name: 'course2'},
-                { id: 3, name: 'course3'}
-              ];
-  }
-
-  trackCourse(index, course){
-    return course ? course.id : undefined;
-  }
+  canSave=true;
 
 }
