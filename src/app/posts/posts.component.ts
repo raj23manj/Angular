77d8c6e966c795e46c5dc15e7478020db6633d56
rajesh23-drg <rmanjunath@teamdrg.com>
2018-02-01 11:14:01 +0530

    handling exception errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -15,12 +15,15 @@ export class PostsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.service.getPosts().subscribe( response => {
+    this.service.getPosts().subscribe(
+      response => {
       this.posts = response.json();
-    }, error => {
-      alert('An Unexpected Error Occurred.');
-      console.log('error');
-    });
+      },
+      error => {
+        alert('An Unexpected Error Occurred.');
+        console.log('error');
+      }
+    );
   }
 
   createPost(input: HTMLInputElement) {
@@ -28,37 +31,53 @@ export class PostsComponent implements OnInit {
     let post = {title: input.value };
     input.value = '';
     this.service.createPost(JSON.stringify(post))
-      .subscribe((response) => {
-        // post.id = response.json().id;
-        post['id'] = response.json().id;
-        // to push at the top of the list
-        this.posts.splice(0, 0, post)
-      }, error => {
-        alert('An Unexpected Error Occurred.');
-        console.log('error');
-      });
+      .subscribe(
+        response => {
+          // post.id = response.json().id;
+          post['id'] = response.json().id;
+          // to push at the top of the list
+          this.posts.splice(0, 0, post)
+        },
+        (error: Response) => {
+          if(error.status === 400){
+            //this.form.setErrors(error.json())
+          }
+          else
+            alert('An Unexpected Error Occurred.');
+          console.log('error');
+        }
+      );
   }
 
   updatePost(post) {
     this.service.editPost(post, JSON.stringify({isRead: true}))
-        .subscribe(response => {
-          console.log(response);
-        }, error => {
-          alert('An Unexpected Error Occurred.');
-          console.log('error');
-        });
+        .subscribe(
+          response => {
+            console.log(response);
+          },
+          error => {
+            alert('An Unexpected Error Occurred.');
+            console.log('error');
+          }
+        );
     //this.http.patch(this.url, JSON.stringify(post));
   }
 
   detelePost(post) {
     this.service.deletePost(post)
-        .subscribe(response => {
-          let index = this.posts.indexOf(post);
-          this.posts.splice(index, 1)
-        }, error => {
-          alert('An Unexpected Error Occurred.');
-          console.log('error');
-        });
+        .subscribe(
+          response => {
+            let index = this.posts.indexOf(post);
+            this.posts.splice(index, 1)
+          },
+          (error: Response) => {
+            if(error.status === 404)
+                alert('Post Already Deleted');
+            else
+              alert('An Unexpected Error Occurred.');
+            console.log('error');
+          }
+        );
   }
 
 }
