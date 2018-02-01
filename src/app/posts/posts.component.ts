75a7c6fc058b621e982c022f1e7151ea6a96bfd8
rajesh23-drg <rmanjunath@teamdrg.com>
2018-02-01 08:32:13 +0530

    Updating data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -31,4 +31,12 @@ export class PostsComponent{
         });
   }
 
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+        .subscribe(response => {
+          console.log(response);
+        });
+    //this.http.patch(this.url, JSON.stringify(post));
+  }
+
 }
