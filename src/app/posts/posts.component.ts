31696c399ce9f8fed9e700ab368e46a648d5e06d
rajesh23-drg <rmanjunath@teamdrg.com>
2018-02-01 08:24:55 +0530

    creating data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,12 +9,26 @@ import { Http } from '@angular/http'
 
 export class PostsComponent{
   posts: any[];
+  private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http) {
-    http.get('http://jsonplaceholder.typicode.com/posts')
+  constructor(private http: Http) {
+    http.get(this.url)
         .subscribe((response => {
           this.posts = response.json();
         }));
+  }
+
+  createPost(input: HTMLInputElement) {
+    //let post: any = {title: input.value };
+    let post = {title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+        .subscribe((response) => {
+          // post.id = response.json().id;
+          post['id'] = response.json().id;
+          // to push at the top of the list
+          this.posts.splice(0, 0, post)
+        });
   }
 
 }
